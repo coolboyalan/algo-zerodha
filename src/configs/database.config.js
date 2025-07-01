3ee69b03ac30d894fea05ec88cefe3781ef174cc
Deepak Singh <deepaky954000@gmail.com>
2025-07-01 11:14:14 +0530

    Update database.config.js

--- src/configs/database.config.js
+++ src/configs/database.config.js
@@ -3,6 +3,7 @@ import { Sequelize } from "sequelize";
 
 const sequelize = new Sequelize(env.DB_NAME, env.DB_USER, env.DB_PASS, {
   host: env.DB_HOST,
+  port: env.DB_PORT,
   dialect: env.DB_DIALECT,
   logging: false,
 });
