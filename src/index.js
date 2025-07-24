7e260088011339ae104c8301c5feab76de3a3bc5
Deepak Singh <deepaky954000@gmail.com>
2025-07-24 09:00:00 +0530

    Added single lot as base order

--- src/index.js
+++ src/index.js
@@ -321,9 +321,9 @@ cron.schedule("* * * * * *", async () => {
         let symbol;
 
         if (direction === "CE") {
-          assetPrice += 300;
+          assetPrice += 200;
         } else if (direction === "PE") {
-          assetPrice -= 300;
+          assetPrice -= 200;
         }
 
         if (direction) {
@@ -421,7 +421,7 @@ cron.schedule("* * * * * *", async () => {
 
             if (direction) {
               ltp = await getLTP(`${symbol.exchange}:${symbol.tradingsymbol}`);
-              noOfLots = Math.floor(usableFunds / (ltp * symbol.lot_size));
+              noOfLots = 1 ?? Math.floor(usableFunds / (ltp * symbol.lot_size));
             }
             const pnl = await getTodaysPnL();
 
