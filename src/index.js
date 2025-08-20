e09d3787188e6b50716226bc441a67f1e2476b32
Deepak Singh <deepaky954000@gmail.com>
2025-08-20 12:14:13 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -318,9 +318,9 @@ cron.schedule("* * * * * *", async () => {
         let symbol;
 
         if (direction === "CE") {
-          assetPrice += 200;
+          assetPrice += 300;
         } else if (direction === "PE") {
-          assetPrice -= 200;
+          assetPrice -= 300;
         }
 
         if (direction) {
